e99e60a4f95e407b597ac015a5448a41db8ba42d
soxft <email@xcsoft.top>
2024-09-09 10:49:20 +0800

    perf: 文字优化

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
-import Image from "next/image";
-
-export default function Home() {
+const Home = () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="flex flex-col gap-4 items-center">
+          <h1 className="text-3xl font-bold text-center sm:text-left">
+            Vercel Fast DNS
+          </h1>
+        </div>
         <ol className="list-inside list-decimal text-md text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li>
-            Bind a custom domain to your Vercel project.
+            为您的 Vercel 项目绑定自定义域名
           </li>
           <li className="my-2">
-            Set up a fast DNS server for cn users. {" "}
+            将 DNS 设置为 {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               cname-vc.9420.ltd
             </code>
           </li>
           <li>
-            Wait for the DNS to take effect.
+            体验针对 CN 用户的 Vercel 分运营商优化 DNS
           </li>
         </ol>
       </main>
@@ -26,16 +29,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Deployed by xcsoft
+          by xcsoft
         </a>
       </footer>
     </div>
   );
 }
+
+
+export default Home;

e827164f2d28e5f1bc5b1908fc59329563b007e7
soxft <email@xcsoft.top>
2024-09-09 09:53:34 +0800

    docs: perf metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";;
 
 export const metadata: Metadata = {
-  title: "vercel fast dns",
-  description: "vercel fast dns for cn users",
+  title: "Vercel FAST DNS",
+  description: "Vercel fast dns for cn users.",
 };
 
 export default function RootLayout({

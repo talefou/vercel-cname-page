5dfc8f389d9915570684f4aed9209745329ce69c
VAYFOU <159092352+talefou@users.noreply.github.com>
2024-10-22 21:10:20 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -36,11 +36,11 @@ const Home = () => {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://www.xsot.cn"
+          href="https://www.vayfou.cn"
           target="_blank"
           rel="noopener noreferrer"
         >
-          xcsoft
+          VAYFOU
         </a>
       </footer>
     </div>
